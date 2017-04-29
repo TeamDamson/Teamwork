9ae96a1b0f3f503919d9750139a6d1e35cd64547
lorashopova <lshopowa@yahoo.com>
2017-04-29 12:26:59 +0300

    reorganize files
    reorganize files

--- scripts/helpers/info.js
+++
@@ -1,8 +0,0 @@
-function displayError(error) {
-    $('#error').show()
-        .text('Error: ' + error.responseJSON.description).hide("fade", 4000);
-}
-
-function showSuccessMessage(message) {
-    $('#info').text(message).show().hide("fade", 4000);
-}
